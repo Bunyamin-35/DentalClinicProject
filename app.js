cd4790e09e1597d81f860fca3f7ed01085b6d268
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-05-06 15:02:47 +0300

    Dental Clinic | 02-Links updated

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set("view engine","ejs");
+//Middlewares
+app.use(express.static("public")); //static files will be in the public file.
+
+// app.get("/",(req,res)=>{
+//     res.status(200).send('INDEXx SAYFASİ') // we will render the index template instead of text
+// }) 
+
+//Routes
 app.get("/",(req,res)=>{
-    res.status(200).send('INDEXx SAYFASİ')
+    res.status(200).render('index',{
+        page_name:"index",
+    })
 })
+app.get("/about",(req,res)=>{
+    res.status(200).render('about',{
+        page_name:"about",
+    })
+})
+
 
 const port = 3000;
 app.listen(port,() =>{
